508a418c5e37c73d5f29693a4a784c750e47fdbf
André Luiz <123383093+Andre2kLuiz@users.noreply.github.com>
2023-07-26 13:18:21 -0300

    up

---
+++ JS/Aula02.js
@@ -0,0 +1,28 @@
+
+
+console.log("Olá Mundo!")
+document.write("<br>Olá Mundo!</br>")
+
+let mensagem = "Minha mensagem"
+let mensagem2 = "Outra mensagem" 
+let meuPeso = 83.5
+
+let minhaAltura = 1.75
+let minhaIdade = 40
+let ehDoador = false
+let teste = null
+
+const pi = 3.1415
+const TAXA = 0.5
+
+/* escrever conteudo de variaveis e constantes*/
+
+document.write(`Mensagem: ${mensagem}`)// teampleatstream
+document.write("<p> Mensagem2: " + mensagem2 + "</p>") // concatenação
+document.write(`Mensagem: ${meuPeso}`)
+document.write("<p>Mensagem: " + minhaAltura + "</p>")
+document.write(`Mensagem: ${minhaIdade}`)
+document.write("<p>Mensagem " + ehDoador + "</p>")
+document.write(`Mensagem: ${teste}`)
+document.write("<p>Mensagem: " + pi + "</p>")
+document.write(`Mensagem: ${TAXA}`)
---
+++ JS/Aula03.js
@@ -0,0 +1,33 @@
+/*Arquivo JS aula 03 Operadores e Operações */
+
+let n1 = 10 //numero interio
+let n2 = 2 //numero inteiro
+let n3 = "2" //string texto
+
+document.write(`N1: ${n1} <br>`)
+document.write(`N1: ${n2} <br>`)
+document.write(`N1: ${n3} <br>`)
+
+console.log(n1 + n2)// adição
+console.log(n1 - n2)// subtração
+console.log(n1 * n2)// multiplicação
+console.log(n1 / n2)// divisão
+console.log(n1 % n2)//resto ou modulo
+
+console.log(n2 == n3)// igual a
+console.log(n2 === n3)//identico a
+console.log(n2 != n3)//diferente de não =
+console.log(n2 !== n3)//não identico
+
+console.log(n1 > n2)// maior que
+console.log(n1 < n2)// menor que
+console.log(n1 >= n2)// maior ou igual
+console.log(n1 <= n2)// menor ou igual
+
+let a = true
+let b = false
+
+console.log(a && b)// E AND
+console.log(a || b)// OU OR
+console.log(!a)// NÃO NOT negação
+console.log(!b)
